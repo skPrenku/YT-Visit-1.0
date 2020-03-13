b4846b9c2232040227dc14839ec862a5c0064624
ilir prenku <ilirprenku@hotmail.com>
2020-03-13 20:41:56 +0100

    Add files via upload

---
+++ YT Visit/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace YT_Visit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.listFile = new System.Windows.Forms.ListBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnBrows = new System.Windows.Forms.Button();
+            this.txtTimeToVisit = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTimeToRenew = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(378, 383);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // listFile
+            // 
+            this.listFile.FormattingEnabled = true;
+            this.listFile.Location = new System.Drawing.Point(12, 58);
+            this.listFile.Name = "listFile";
+            this.listFile.Size = new System.Drawing.Size(441, 277);
+            this.listFile.TabIndex = 1;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(378, 341);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 20);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(12, 341);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(248, 20);
+            this.txtFilePath.TabIndex = 3;
+            // 
+            // btnBrows
+            // 
+            this.btnBrows.Location = new System.Drawing.Point(266, 341);
+            this.btnBrows.Name = "btnBrows";
+            this.btnBrows.Size = new System.Drawing.Size(47, 20);
+            this.btnBrows.TabIndex = 4;
+            this.btnBrows.Text = "Browse";
+            this.btnBrows.UseVisualStyleBackColor = true;
+            this.btnBrows.Click += new System.EventHandler(this.BtnBrows_Click);
+            // 
+            // txtTimeToVisit
+            // 
+            this.txtTimeToVisit.Location = new System.Drawing.Point(120, 10);
+            this.txtTimeToVisit.Name = "txtTimeToVisit";
+            this.txtTimeToVisit.Size = new System.Drawing.Size(24, 20);
+            this.txtTimeToVisit.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Visit Time in (s): ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtTimeToRenew);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtTimeToVisit);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(465, 38);
+            this.panel1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(249, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "DHCP Renew every (h):";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtTimeToRenew
+            // 
+            this.txtTimeToRenew.Location = new System.Drawing.Point(410, 8);
+            this.txtTimeToRenew.Name = "txtTimeToRenew";
+            this.txtTimeToRenew.Size = new System.Drawing.Size(24, 20);
+            this.txtTimeToRenew.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(13, 382);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "www.iliri.net";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 416);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnBrows);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.listFile);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YT Visit 1.0";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox listFile;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnBrows;
+        private System.Windows.Forms.TextBox txtTimeToVisit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTimeToRenew;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ YT Visit/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.Configuration;
+using System.Diagnostics;
+
+
+
+
+
+namespace YT_Visit
+{
+    public partial class Form1 : Form
+    {
+        
+        private static void Run(int hrs)
+        {
+            
+            int seconds = hrs*60*60 * 1000;
+
+            var timer = new System.Threading.Timer(TimerMethod, null, 0, seconds);
+        }
+
+        private static void TimerMethod(object o)
+        {
+            Process.Start("renewip.cmd");
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+         
+
+        }
+
+        public async Task VisitLinesSlowly()
+        {
+            while (true)
+            {
+                foreach (string item in listFile.Items)
+                {
+                    visitUrl(item + "?autoplay=1", int.Parse(txtTimeToVisit.Text));
+                    await Task.Delay(3 * 1000);
+
+                }
+            }
+            
+        }
+
+
+        public void visitUrl(string url, int ct)
+        {
+            var prs = new ProcessStartInfo("firefox.exe");
+            prs.Arguments = url;   // + " --incognito";
+            Process.Start(prs);
+          
+
+            Thread.Sleep(ct * 1000);
+            try
+            {
+                foreach (Process proc in Process.GetProcessesByName("firefox"))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+        }
+
+
+        private void BtnBrows_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog op = new OpenFileDialog();
+              //  op.ShowDialog();
+                if (op.ShowDialog() == DialogResult.OK)
+                    txtFilePath.Text = op.FileName;
+            }
+            catch ( Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(txtFilePath.Text.Trim());
+                foreach (string line in lines)
+                {
+                    listFile.Items.Add(line);
+                }
+
+                
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+           
+        }
+
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+           
+                int time = int.Parse(txtTimeToRenew.Text);
+                
+                Run(time);
+            
+                VisitLinesSlowly();
+            
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.iliri.net/");
+        }
+    }
+}
